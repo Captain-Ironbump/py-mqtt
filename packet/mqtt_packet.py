a5f31a2bdbc8e8c18d91ede4553c6ef6e25fd726
Lukas Linde <linde.lukas.99@gmail.com>
2024-03-10 17:20:07 +0100

    decode and encode class functions in MqttPacket class

--- packet/mqtt_packet.py
+++ packet/mqtt_packet.py
@@ -1,7 +1,6 @@
 import ctypes
 import miscs
 
-
 class MqttPacket(ctypes.Union):
     _fields_ = [("ack", miscs.MqttAck),
                 ("header", miscs.MQTTHeader),
@@ -11,4 +10,44 @@ class MqttPacket(ctypes.Union):
                 ("publish", miscs.MqttPublish),
                 ("subscribe", miscs.MqttSubscribe),
                 ("unsubscribe", miscs.MqttUnsubscribe)]
+    _MAX_LEN_BYTES = 4
+    
+    def encode_lenght(self, length):
+        bytes = 0
+        buf = bytearray(MqttPacket._MAX_LEN_BYTES)
+        while True:
+            if bytes + 1 > MqttPacket._MAX_LEN_BYTES:
+                return buf[:bytes]
+            d = length % 128
+            length //= 128
+            if length > 0:
+                d |= 128
+            buf[bytes] = d
+            bytes += 1
+            if length == 0:
+                return buf[:bytes]
+    
+
+    def decode_lenght(self, buf_ptr):
+        """ 
+        Decode a length from the buffer.
+        The parameter buf_ptr is a pointer to the buffer.
+        The provided buffer should be parssed as a bytearray.
+
+        Returns the length and the number of bytes read.
+        """
+        multiplier = 1
+        value = 0
+        index = 0
+        buf = ctypes.cast(buf_ptr, ctypes.POINTER(ctypes.c_ubyte))
+        while True:
+            c = buf[index]
+            value += (c & 127) * multiplier
+            multiplier *= 128
+            index += 1
+            if (c & 128) == 0:
+                break
+            if index > len(buf):
+                raise ValueError("Buffer too short for decoding length")
+        return value, index
     
