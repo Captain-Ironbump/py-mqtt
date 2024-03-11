cef895cd28fdfd5d711eea1d4f7b8b97dc716df5
Lukas Linde <linde.lukas.99@gmail.com>
2024-03-11 22:27:43 +0100

    mqtt header and variable header with type dependent constructor

--- packet/miscs/mqtt_header.py
+++ packet/miscs/mqtt_header.py
@@ -1,46 +1,53 @@
 class MqttFixedHeader():
     __slots__ = ('package_type', 'remaining_length', 'flags') # flags -> retein, qos, qos, dup -> 2^4
 
-    _packet_types = {
-        'CONNECT': 1,
-        'CONNACK': 2,
-        'PUBLISH': 3,
-        'PUBACK': 4,
-        'PUBREC': 5,
-        'PUBREL': 6,
-        'PUBCOMP': 7,
-        'SUBSCRIBE': 8,
-        'SUBACK': 9,
-        'UNSUBSCRIBE': 10,
-        'UNSUBACK': 11,
-        'PINGREQ': 12,
-        'PINGRESP': 13,
-        'DISCONNECT': 14
-    }
-
     _packet_type_flags = {
-        'CONNECT': 0,
-        'CONNACK': 0,
-        'PUBLISH': 0,
-        'PUBACK': 0,
-        'PUBREC': 0,
-        'PUBREL': 2,
-        'PUBCOMP': 0,
-        'SUBSCRIBE': 2,
-        'SUBACK': 0,
-        'UNSUBSCRIBE': 2,
-        'UNSUBACK': 0,
-        'PINGREQ': 0,
-        'PINGRESP': 0,
-        'DISCONNECT': 0
+        'CONNECT': (1, 0),
+        'CONNACK': (2, 0),
+        'PUBLISH': (3, {
+            'AT_MOST_ONCE': 0,
+            'AT_LEAST_ONCE': 1,
+            'EXACTLY_ONCE': 2
+        }),
+        'PUBACK': (4, 0),
+        'PUBREC': (5, 0),
+        'PUBREL': (6, 2),
+        'PUBCOMP': (7, 0),
+        'SUBSCRIBE': (8, 2),
+        'SUBACK': (9, 0),
+        'UNSUBSCRIBE': (10, 2),
+        'UNSUBACK': (11, 0),
+        'PINGREQ': (12, 0),
+        'PINGRESP': (13, 0),
+        'DISCONNECT': (14, 0)
     }
 
     def __init__(self, package_type: str, lenght=0) -> None:
-        self.package_type = package_type if package_type in MqttFixedHeader._packet_type_flags.keys() else None # x if x > y else y
+        try:
+            self.package_type = (package_type, MqttFixedHeader._packet_type_flags[package_type][0])
+        except KeyError:
+            raise ValueError(f"Invalid package type: {package_type}")
+        self.flags = MqttFixedHeader._packet_type_flags[package_type][1] if package_type != 'PUBLISH' else MqttFixedHeader._packet_type_flags[package_type]['AT_MOST_ONCE']
         self.remaining_length = lenght
-        self.flags = MqttFixedHeader._packet_type_flags[package_type]
         
 
 class MqttVariableHeader():
-    def __init__(self) -> None:
-        pass
+    __packet_type_flags = {
+        'CONNECT': ('bits', 'keep_alive', 'client_id', 'username', 'password', 'will_topic', 'will_message'),
+        'CONNACK': ('bits', 'rc'),
+        'PUBLISH': ('topic_len', 'topic_ptr', 'packet_id'),
+        'PUBACK': ('packet_id',),
+        'PUBREC': ('packet_id',),
+        'PUBREL': ('packet_id',),
+        'PUBCOMP': ('packet_id',),
+        'SUBSCRIBE': ('packet_id',),
+        'SUBACK': ('packet_id',),
+        'UNSUBSCRIBE': ('packet_id',),
+        'UNSUBACK': ('packet_id',),
+        'PINGREQ': (),
+        'PINGRESP': (),
+        'DISCONNECT': ()
+    }
+    
+    def __init__(self, package_type: str) -> None:
+        self.__slots__ = MqttVariableHeader.__packet_type_flags[package_type]
